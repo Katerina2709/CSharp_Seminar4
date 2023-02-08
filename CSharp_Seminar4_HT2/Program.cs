bf19fa7bdf4eab5168c5c5239d743d88e63cebf3
Olga19bon <slizkovaev@mail.ru>
2023-02-09 02:46:43 +0300

    Решение задачи 27

---
+++ CSharp_Seminar4_HT2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и 
+// выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int SumNumb (int numb)
+{
+   int sum = 0;
+   while (numb > 0)
+   {
+        sum = sum + numb % 10;
+        numb = numb / 10;
+   }
+   return sum;
+}
+
+Console.Write ("Введите число: ");
+int number = Convert.ToInt32 (Console.ReadLine ());
+Console.WriteLine ( $"Сумма цифр в числе {number} -> {SumNumb (Math.Abs (number))}" ); 
+// число может быть отрицательным, поэтому берем его модуль
