58ff6f070f324e64957001e3412f7ff312f95187
Olga19bon <slizkovaev@mail.ru>
2023-02-11 03:44:10 +0300

    Решение задачи 29 (вариант 2: ввод элементов массива с консоли)

---
+++ CSharp_Seminar4_HT3_2/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов 
+// и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+void PrintArray (int[] array) // вывод элементов массива на экран
+{
+    Console.Write ("[");
+    foreach (var item in array)
+    {
+        Console.Write ($"{item}, ");
+    }
+     Console.Write ("\b\b]");
+}
+
+Console.Write ("Введите длину массива: ");
+int leng = Convert.ToInt32(Console.ReadLine());
+if (leng > 0 )
+{
+   int[] array = new int[leng];
+   for (int i = 0; i < leng; i++)
+   { 
+        Console.Write ($"Введите {i} элемент массива: ");
+        array [i] = Convert.ToInt32(Console.ReadLine());
+   }
+   PrintArray (array); // вывод на экран. 
+}
+else
+{
+    Console.Write ("Длина массива должна быть > 0");
+}
+     
+
